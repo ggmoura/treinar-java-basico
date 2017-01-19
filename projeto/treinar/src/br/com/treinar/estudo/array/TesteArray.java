8dd91e139569879738125b1f119788f25d15a1a5
ADM <ADM@SL03PROFESSOR>
2017-01-19 20:26:15 -0200

    array

---
+++ projeto/treinar/src/br/com/treinar/estudo/array/TesteArray.java
@@ -0,0 +1,16 @@
+package br.com.treinar.estudo.array;
+
+public class TesteArray {
+
+	public static void main(String[] args) {
+		String[] nomes = new String[3];
+		
+		nomes[0] = "Gleidson";
+		nomes[1] = "Karina";
+		nomes[2] = "Jadine";
+		
+		System.out.println(nomes);
+		
+	}
+	
+}
