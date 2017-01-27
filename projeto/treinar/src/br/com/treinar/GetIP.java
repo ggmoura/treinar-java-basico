a10f1433368c86dc87c7766f71312f99e6a976a8
ADM <ADM@SL03PROFESSOR>
2017-01-27 18:46:10 -0200

    TESTE

---
+++ projeto/treinar/src/br/com/treinar/GetIP.java
@@ -0,0 +1,28 @@
+package br.com.treinar;
+
+import java.net.*;
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class GetIP {
+	public static void main(String[] arguments) {
+		String systemipaddress = "";
+		try {
+			URL url_name = new URL("http://bot.whatismyipaddress.com");
+			BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()));
+			systemipaddress = sc.readLine().trim();
+			if (!(systemipaddress.length() > 0)) {
+				try {
+					InetAddress localhost = InetAddress.getLocalHost();
+					System.out.println((localhost.getHostAddress()).trim());
+					systemipaddress = (localhost.getHostAddress()).trim();
+				} catch (Exception e1) {
+					systemipaddress = "Cannot Execute Properly";
+				}
+			}
+		} catch (Exception e2) {
+			systemipaddress = "Cannot Execute Properly";
+		}
+		System.out.println("\nYour IP Address: " + systemipaddress + "\n");
+	}
+}
