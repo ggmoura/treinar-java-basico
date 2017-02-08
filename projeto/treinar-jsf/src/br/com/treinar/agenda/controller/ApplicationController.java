90b6d4c8e596582905566cfa3b818a7d2fee7a9a
ADM <ADM@SL03PROFESSOR>
2017-02-07 22:35:36 -0200

    teste

--- projeto/treinar-jsf/src/br/com/treinar/agenda/controller/ApplicationController.java
+++ projeto/treinar-jsf/src/br/com/treinar/agenda/controller/ApplicationController.java
@@ -7,24 +7,35 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
+import br.com.treinar.agenda.modelo.Sexo;
 import br.com.treinar.agenda.modelo.TipoTelefone;
 
 @ManagedBean(name = "agendaApplication")
 public class ApplicationController {
 
 	private List<TipoTelefone> tiposTelefones;
+	private List<Sexo> sexos;
 
 	@PostConstruct
 	private void init() {
 		tiposTelefones = new ArrayList<>(Arrays.asList(TipoTelefone.values()));
+		sexos = new ArrayList<>(Arrays.asList(Sexo.values()));
 	}
-	
+
 	public List<TipoTelefone> getTiposTelefones() {
 		return tiposTelefones;
 	}
 
 	public void setTiposTelefones(List<TipoTelefone> tiposTelefones) {
 		this.tiposTelefones = tiposTelefones;
+	}
+
+	public List<Sexo> getSexos() {
+		return sexos;
+	}
+
+	public void setSexos(List<Sexo> sexos) {
+		this.sexos = sexos;
 	}
 
 }
